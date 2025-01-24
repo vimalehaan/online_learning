2068735f33ec42c1da0f635db89c0641fc5878a8
vimalehaan <vimalehaan2312@mail.com>
2025-01-24 15:25:34 +0530

    cors error fixed

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,21 @@ const authRoutes = require("./routes/authRoutes");
 const courseRoutes = require("./routes/courseRoutes");
 const openaiRoutes = require("./routes/openaiRoutes");
 
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
 
+const react_app_url = process.env.REACT_APP_BASE_URL
+
+app.use(cors({
+    origin: `${react_app_url}`,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+app.options('*', cors());
+
 app.use(express.json());
-app.use(cors());
 
 app.use("/auth", authRoutes);
 app.use("/courses", courseRoutes);
